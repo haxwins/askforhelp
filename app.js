bf8ad4bc5f99d0b25dff5763e73f03f61d00406f
haxwins <haxregist@gmail.com>
2018-03-09 17:07:56 +0100

    1.0

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ let port = 3000;
 
 Post = require('./models/post');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/askforhelp');
 let db = mongoose.connection;
